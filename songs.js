7096d68fd158d117bcf36ead262a045c261abecd
Turizam123 <nssr@abv.bg>
2026-08-04 19:05:11 +0300

    Add songs.js with YouTube links

---
+++ songs.js
@@ -0,0 +1,16 @@
+// ======================================================
+// Angel Uzunov Artist Website
+// songs.js
+// Добавяй само нов YouTube линк в края на списъка.
+// ======================================================
+
+const youtubeSongs = [
+  "https://www.youtube.com/watch?v=mHK8o9dvHks",
+  "https://www.youtube.com/watch?v=xvgsuSr6hB8",
+  "https://www.youtube.com/watch?v=Y8y7K8PmVwM",
+  "https://www.youtube.com/watch?v=yOIzCb_R2hw",
+  "https://www.youtube.com/watch?v=gpLYgQaU1Mk",
+  "https://www.youtube.com/watch?v=MLCLaXaSneI",
+  "https://www.youtube.com/watch?v=z6I9QPKcCcY",
+  "https://www.youtube.com/watch?v=oKCgChl8jOo"
+];
